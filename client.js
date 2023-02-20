0eef831b4375db5112b5b626e8e424e6b1cbac0d
iyese <iyese27@gmail.com>
2023-02-21 01:17:28 +0300

    websockets set up

---
+++ client.js
@@ -0,0 +1,9 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8081');
+
+ws.on('open', function open() {
+    ws.on('message', function message(data) {
+      console.log(`${data}`);
+    });
+});
